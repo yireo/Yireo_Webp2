d7286add22aecbf58010d1e19405a85b2e9aad6d
Jisse Reitsma <info@yireo.com>
2019-03-21 15:16:26 +0100

    Pending changes

---
+++ view/frontend/web/js/detect.js
@@ -0,0 +1,28 @@
+/**
+ * Yireo Webp2 for Magento
+ *
+ * @author      Yireo (https://www.yireo.com/)
+ * @copyright   Copyright (c) 2019 Yireo (https://www.yireo.com/)
+ * @license     GNU General Public License
+ */
+
+define([
+    'jquery'
+], function ($) {
+
+    return function(config) {
+        if ($.cookie('webp')) {
+            return true;
+        }
+
+        var Tester = new Image();
+        Tester.onload = function () {
+            if (Tester.width > 0 && Tester.height > 0) {
+                console.log('Set cookie');
+                document.cookie = 'webp=1';
+            }
+        };
+
+        Tester.src = config.image;
+    };
+});
