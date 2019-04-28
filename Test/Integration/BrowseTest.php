a90b9d92c75298786aabba1507b8988ffb8bb523
Jisse Reitsma <jisse@yireo.com>
2019-04-28 13:16:36 +0200

    Actual meaningful integration test for browsing product page

--- Test/Integration/BrowseTest.php
+++ Test/Integration/BrowseTest.php
@@ -12,22 +12,50 @@ use Magento\TestFramework\TestCase\AbstractController;
 class BrowseTest extends AbstractController
 {
     /**
+     * @magentoDataFixture Magento/Catalog/_files/product_simple_with_image.php
      * @magentoAdminConfigFixture system/yireo_webp/enabled 1
      */
     public function testIfHtmlContainsWebpImages()
     {
-        $this->dispatch('/');
+        $this->dispatch('/catalog/product/view/id/1');
         $body = $this->getResponse()->getBody();
-        $this->assertContains('type="image/webp"', $body);
+
+        if ($this->checkBodyForValidImgTags($body)) {
+            $this->assertContains('type="image/webp"', $body);
+            return;
+        }
+
+        $this->markTestSkipped('No replaceable <img> tags found in body');
     }
 
     /**
+     * @magentoDataFixture Magento/Catalog/_files/product_simple_with_image.php
      * @magentoAdminConfigFixture system/yireo_webp/enabled 0
      */
     public function testIfHtmlContainsNoWebpImages()
     {
-        $this->dispatch('/');
+        $this->dispatch('/catalog/product/view/id/1');
         $body = $this->getResponse()->getBody();
-        $this->assertNotContains('type="image/webp"', $body);
+
+        if ($this->checkBodyForValidImgTags($body)) {
+            $this->assertNotContains('type="image/webp"', $body);
+            return;
+        }
+
+        $this->markTestSkipped('No replaceable <img> tags found in body: '.$body);
+    }
+
+    /**
+     * @param string $body
+     * @return bool
+     */
+    private function checkBodyForValidImgTags(string $body): bool
+    {
+        $body = str_replace("\n", "", $body);
+        if (preg_match('/<img\ (.*)src="(.*)\.(jpg|png)"([^>]+)>/', $body)) {
+            return true;
+        }
+
+        return false;
     }
 }
