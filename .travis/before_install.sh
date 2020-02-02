06a2f212a3939ce3c934e048c1c81f6b5f63b314
Jisse Reitsma <jisse@yireo.com>
2020-02-02 14:06:03 +0530

    Re-add

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -11,6 +11,8 @@ fi
 if [[ ${TEST_SUITE} != "unit" ]]; then
   echo > ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/xdebug.ini
 fi
+
 echo 'memory_limit = -1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+
 phpenv rehash;
 
