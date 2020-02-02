508183611640be5799bfa2ebc22dc0f8d3b8ad5a
Jisse Reitsma <jisse@yireo.com>
2020-02-02 13:14:44 +0530

    Add files for running integration tests

---
+++ .magento/dev/tests/integration/etc/install-config-mysql.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+return [
+    'db-host' => '127.0.0.1',
+    'db-user' => 'root',
+    'db-password' => 'root',
+    'db-name' => 'magento2',
+    'db-prefix' => '',
+    'backend-frontname' => 'backend',
+    'admin-user' => \Magento\TestFramework\Bootstrap::ADMIN_NAME,
+    'admin-password' => \Magento\TestFramework\Bootstrap::ADMIN_PASSWORD,
+    'admin-email' => \Magento\TestFramework\Bootstrap::ADMIN_EMAIL,
+    'admin-firstname' => \Magento\TestFramework\Bootstrap::ADMIN_FIRSTNAME,
+    'admin-lastname' => \Magento\TestFramework\Bootstrap::ADMIN_LASTNAME,
+];
