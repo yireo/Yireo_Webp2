dd1d9ba6c66767e3f917f27456e1f52b6d191bd4
Jisse Reitsma <jisse@yireo.com>
2020-02-03 21:24:59 +0530

    Correct test for catting log files

--- .travis/after_script.sh
+++ .travis/after_script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 find /tmp/magento2/var/report -type f -exec cat {} \;
-test /tmp/magento2/var/log/exception.log && cat /tmp/magento2/var/log/exception.log
-test /tmp/magento2/var/log/system.log && cat /tmp/magento2/var/log/system.log
+test -f /tmp/magento2/var/log/exception.log && cat /tmp/magento2/var/log/exception.log
+test -f /tmp/magento2/var/log/system.log && cat /tmp/magento2/var/log/system.log
