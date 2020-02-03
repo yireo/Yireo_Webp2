caacc20f39ab395a2b65c89454992cbb21be5f43
Jisse Reitsma <jisse@yireo.com>
2020-02-03 21:18:20 +0530

    Initialize simple unit testing

--- .travis/scripts/integration-tests.sh
+++ .travis/scripts/integration-tests.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 pushd /tmp/magento2
 
+test -f dev/tests/integration/phpunit.xml || exit 0
+
 echo 'Creating magento-integration-test database'
 mysql -u root --password=root -e 'CREATE DATABASE `magento-integration-test`;'
 echo "USE mysql;\nUPDATE user SET password=PASSWORD('root') WHERE user='root';\nFLUSH PRIVILEGES;\n" | mysql -u root
--- .travis/scripts/unit-tests.sh
+++ .travis/scripts/unit-tests.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
-composer require "mustache/mustache":"~2.5"
-composer require "php-coveralls/php-coveralls":"^1.0"
+pushd /tmp/magento2
+
+test -f dev/tests/unit/phpunit.xml || exit 0
+
+#composer require "mustache/mustache":"~2.5"
+#composer require "php-coveralls/php-coveralls":"^1.0"
+
+echo "Running unit tests"
+cd dev/tests/unit/
+../../../vendor/bin/phpunit -c phpunit.xml
+
+popd
+    
