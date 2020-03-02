cfc5c32b965f916b6ce278be2533e86779cd763c
Jisse Reitsma <jisse@yireo.com>
2020-03-02 20:21:59 +0100

    Add a simple unit test to test file resolving

---
+++ Test/Unit/Image/FileTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Magento\Framework\Filesystem\Directory\ReadFactory;
+use Magento\Framework\Filesystem\DirectoryList;
+use Yireo\Webp2\Image\File;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class FileTest testing behaviour of File
+ */
+class FileTest extends TestCase
+{
+    /**
+     * @test Test the resolve() function
+     */
+    public function testResolve()
+    {
+        $target = $this->getTarget();
+        $resolvedFile = $target->resolve('http://anotherhost.com/some/fake/url.png');
+        $this->assertSame($resolvedFile, '/pub/some/fake/url.png');
+    }
+
+    /**
+     * @return File
+     */
+    public function getTarget(): File
+    {
+        $directoryListMock = $this->getMockBuilder(DirectoryList::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $readFactoryMock = $this->getMockBuilder(ReadFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $target = new File($directoryListMock, $readFactoryMock);
+        return $target;
+    }
+}
