97aa70682d74c24922bd14e88a180dd917795f74
Johannes Smit <johannes.smit@mediact.nl>
2020-08-19 10:19:31 +0200

    Fixes wrong name in require-js config

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -5,7 +5,7 @@ var config = {
     config: {
         mixins: {
             'Magento_Swatches/js/swatch-renderer': {
-                'Yireo_Webp2/js/webp-swatch-renderer': true
+                'Yireo_Webp2/js/swatch-renderer': true
             },
         }
     }
