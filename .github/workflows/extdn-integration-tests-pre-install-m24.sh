a4596f9309e26bbadb20748f68e30768f21f01e6
Jisse Reitsma <jisse@yireo.com>
2021-08-10 15:57:09 +0200

    Remove dummy repo

--- .github/workflows/extdn-integration-tests-pre-install-m24.sh
+++ .github/workflows/extdn-integration-tests-pre-install-m24.sh
@@ -3,4 +3,4 @@ composer config minimum-stability dev
 composer config prefer-stable false
 
 composer require yireo/magento2-replace-bundled:^4.0 --no-update
-composer require yireo/magento2-replace-test:@dev --no-update
+#composer require yireo/magento2-replace-test:@dev --no-update
