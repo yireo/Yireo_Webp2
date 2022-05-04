405c3eb32dec6fac584f53eb0301f1eb6b04594c
jisse Reitsma <jisse@yireo.com>
2022-05-04 18:57:33 +0200

    Repair workflows

--- .github/workflows/extdn-integration-tests-pre-install-m23.sh
+++ .github/workflows/extdn-integration-tests-pre-install-m23.sh
@@ -3,4 +3,3 @@ composer config minimum-stability dev
 composer config prefer-stable false
 
 composer require yireo/magento2-replace-bundled:^3.0 --no-update
-#composer require yireo/magento2-replace-test:@dev --no-update
