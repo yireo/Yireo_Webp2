da8e18436d05a2d3c5476f5f3c417ba06fbf5538
jisse Reitsma <jisse@yireo.com>
2023-06-14 12:07:21 +0200

    Prettier

--- GraphQl/Resolver/WebpUrl.php
+++ GraphQl/Resolver/WebpUrl.php
@@ -6,10 +6,10 @@ use Magento\CatalogGraphQl\Model\Resolver\Product\MediaGallery\Url as UrlResolve
 use Magento\Framework\Exception\FileSystemException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 use Magento\Framework\GraphQl\Query\Resolver\Value;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Magento\TestFramework\Helper\Bootstrap;
 use Yireo\NextGenImages\Config\Config;
 use Yireo\NextGenImages\Exception\ConvertorException;
 use Yireo\NextGenImages\Image\ImageFactory;
@@ -56,8 +56,10 @@ class WebpUrl implements ResolverInterface
     }
 
     /**
+     * Resolve this GraphQL request
+     *
      * @param Field $field
-     * @param $context
+     * @param ContextInterface $context
      * @param ResolveInfo $info
      * @param array|null $value
      * @param array|null $args
