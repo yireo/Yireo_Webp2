15dbc69e3435aeb1db51d592190e791f379e4509
Jisse Reitsma <jisse@yireo.com>
2024-08-23 11:59:36 +0200

    Pending changes

---
+++ .github/workflows/extdn-phpstan-pre-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+composer config minimum-stability dev
+composer config prefer-stable false
+
+composer require --dev yireo/magento2-integration-test-helper --no-update
+
+composer config --no-plugins allow-plugins true
+composer require --dev phpstan/extension-installer --no-update
+composer require --dev bitexpert/phpstan-magento --no-update
+
+composer require yireo/magento2-replace-bundled:^4.0 --no-update
+composer require yireo/magento2-replace-inventory:^4.0 --no-update
+composer require yireo/magento2-replace-pagebuilder:^4.0 --no-update
