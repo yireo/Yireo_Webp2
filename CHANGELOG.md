7b0dcc4624d313cb0ca961e8c29dcf0d9f23bdf1
Jisse Reitsma <jisse@yireo.com>
2024-08-23 11:39:27 +0200

    Add CSP nonces to inline scripts

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.14.1] - 23 August 2024
+### Fixed
+- Add CSP nonces to inline scripts
+
 ## [0.14.0] - 20 June 2024
 ### Fixed
 - Move GraphQL support in seperate [module](https://github.com/yireo/Yireo_Webp2GraphQl)
---
+++ Plugin/AddCspInlineScripts.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Yireo\Webp2\Plugin;
+
+use Magento\Framework\View\Element\Template;
+use Yireo\CspUtilities\Util\ReplaceInlineScripts;
+
+class AddCspInlineScripts
+{
+    private ReplaceInlineScripts $replaceInlineScripts;
+
+    public function __construct(
+        ReplaceInlineScripts $replaceInlineScripts
+    ) {
+        $this->replaceInlineScripts = $replaceInlineScripts;
+    }
+
+    public function afterToHtml(Template $block, $html): string
+    {
+        if (false === strstr((string)$block->getNameInLayout(), 'yireo_webp2.')) {
+            return $html;
+        }
+
+        return $this->replaceInlineScripts->replace((string)$html);
+    }
+}
