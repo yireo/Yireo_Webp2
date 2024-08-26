11da598e4d8ac5dd3b2183cdbebae90df7b892f3
Jisse Reitsma <jisse@yireo.com>
2024-08-26 09:21:32 +0200

    Rerun GitHub CI

--- .github/workflows/extdn-integration-tests-pre-install.sh
+++ .github/workflows/extdn-integration-tests-pre-install.sh
@@ -5,5 +5,4 @@ composer config prefer-stable false
 composer require --dev yireo/magento2-integration-test-helper --no-update
 
 composer require yireo/magento2-replace-bundled:^4.0 --no-update
-composer require yireo/magento2-replace-inventory:^4.0 --no-update
 composer require yireo/magento2-replace-pagebuilder:^4.0 --no-update
